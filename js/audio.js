f4a88c5eef43454d29d1cdecf2c26e0296411d5e
Filipe Santos <outerspaceguy@live.co.uk>
2022-10-25 17:18:07 -0300

    fixed a bug

--- js/audio.js
+++ js/audio.js
@@ -3,7 +3,7 @@ const buttonPressAudio = new Audio("https://github.com/maykbrito/automatic-video
 const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
 
 const forestSound = new Audio("./audio/Floresta.wav")
-const rainSound = new Audio("./audio/chuva.wav")
+const rainSound = new Audio("./audio/Chuva.wav")
 const fireplace = new Audio("./audio/Lareira.wav")
 const coffeeShop = new Audio("./audio/Cafeteria.wav")
 
